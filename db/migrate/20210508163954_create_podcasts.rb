524944abc4701c8cd4844de86e26cc14979d9012
nick-symon <symon.nick@gmail.com>
2021-05-08 12:51:16 -0500

    create podcast model, init migration

---
+++ db/migrate/20210508163954_create_podcasts.rb
@@ -0,0 +1,23 @@
+class CreatePodcasts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :podcasts, id: false, primary_key: 'id' do |t|
+      t.text :id, null: false 
+      t.text :subtitle, null: false 
+      t.text :rss_feed_link, null: false 
+      t.text :link, null: false 
+      t.text :itunes_categories, null: false, array: true 
+      t.text :image_url, null: false 
+      t.text :image_title, null: false
+      t.text :last_build_date
+      t.text :author_name, null: false
+      t.text :description, null: false
+      t.text :language, null: false
+      t.text :title, null: false
+      t.text :slug, null: false
+      t.text :itunes_subtitle, null: false
+      t.text :itunes_summary, null: false
+      t.text :pub_date
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_05_08_163954) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "podcasts", id: false, force: :cascade do |t|
+    t.text "id", null: false
+    t.text "subtitle", null: false
+    t.text "rss_feed_link", null: false
+    t.text "link", null: false
+    t.text "itunes_categories", null: false, array: true
+    t.text "image_url", null: false
+    t.text "image_title", null: false
+    t.text "last_build_date"
+    t.text "author_name", null: false
+    t.text "description", null: false
+    t.text "language", null: false
+    t.text "title", null: false
+    t.text "slug", null: false
+    t.text "itunes_subtitle", null: false
+    t.text "itunes_summary", null: false
+    t.text "pub_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
