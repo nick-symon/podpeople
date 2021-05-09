4c736fbdc331c16e61bfbc745559d23c568f576f
nick-symon <symon.nick@gmail.com>
2021-05-09 10:37:26 -0500

    additional ep, pod validations

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,4 +2,5 @@ class Episode < ApplicationRecord
   self.primary_key = 'guid'
   belongs_to :podcast
   validates :podcast, presence: true
+  validates :guid, uniqueness: true
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,5 +1,6 @@
 class Podcast < ApplicationRecord
   self.primary_key = 'id'
   has_many :episodes
-  validates :rss_feed_link, :title presence: true
+  validates :rss_feed_link, :title, presence: true
+  validates :rss_feed_link, uniqueness: true
 end
