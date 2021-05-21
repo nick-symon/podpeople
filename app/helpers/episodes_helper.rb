d302a8fcdc2b345f330d67e47381f1197603cd84
nick-symon <symon.nick@gmail.com>
2021-05-21 09:14:54 -0500

    add duration helper to episodes

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -3,4 +3,29 @@ module EpisodesHelper
     parsed_date = DateTime.parse(date)
     parsed_date.strftime("%b %-d, %Y")
   end
+
+  def format_duration(duration)
+    # if : in string, break apart into array
+    if duration.include?(':')
+      time_array = duration.split(':')
+      if time_array.length > 2
+          hour = time_array[0].to_i
+          minute = time_array[1].to_i
+      else
+          minute = time_array[0].to_i
+      end
+      [].tap do |p|
+        p << "#{hour} hour".pluralize(hour) unless hour.zero?
+        p << "#{minute} minute".pluralize(minute) unless minute.zero?
+      end.join(', ')
+    else
+      ActiveSupport::Duration
+        .build(duration.to_i)
+        .parts
+        .except(:seconds)
+        .reduce("") do | output, (key, val) |
+          output += "#{val} #{key}"
+        end.strip
+    end
+  end
 end
