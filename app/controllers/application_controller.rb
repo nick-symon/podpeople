8db32e8671121f3ee4a725c3857de79fe8981c2b
nick-symon <symon.nick@gmail.com>
2021-05-26 13:29:20 -0500

    nested profile attr, add username to sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pagy::Backend
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:username]])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_one :profile
+  has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :profile
+
+  def profile 
+    super || build_profile
+  end
 
 end
