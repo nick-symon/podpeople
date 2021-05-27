a3649ba755ba0a886684c26a37eb52d49eb3fca5
nick-symon <symon.nick@gmail.com>
2021-05-27 12:49:17 -0500

    update review associations

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,6 +1,7 @@
 class Podcast < ApplicationRecord
   self.primary_key = 'id'
   has_many :episodes
+  has_many :reviews
   validates :rss_feed_link, :title, presence: true
   validates :rss_feed_link, uniqueness: true
 
