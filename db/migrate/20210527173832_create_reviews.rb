3df6c67e63196271abed291847cdc9e286ac630e
nick-symon <symon.nick@gmail.com>
2021-05-27 12:48:24 -0500

    update review migration, schema change

--- db/migrate/20210527173832_create_reviews.rb
+++ db/migrate/20210527173832_create_reviews.rb
@@ -1,10 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.integer, :rating
-      t.text, :content
-      t.belongs_to, :user, null: false, foreign_key: true
-      t.belongs_to :podcast, null: false, foreign_key: true
+      t.integer :rating
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :podcast, type: :text, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_180114) do
+ActiveRecord::Schema.define(version: 2021_05_27_173832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,17 @@ ActiveRecord::Schema.define(version: 2021_05_24_180114) do
     t.index ["username"], name: "index_profiles_on_username", unique: true
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.text "podcast_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["podcast_id"], name: "index_reviews_on_podcast_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,4 +95,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_180114) do
 
   add_foreign_key "episodes", "podcasts"
   add_foreign_key "profiles", "users"
+  add_foreign_key "reviews", "podcasts"
+  add_foreign_key "reviews", "users"
 end
