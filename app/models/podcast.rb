42a32fb2445288aacd66957a45cf7ce5b3a403fa
nick-symon <symon.nick@gmail.com>
2021-05-30 10:48:15 -0500

    add likeable associations

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,6 +2,7 @@ class Podcast < ApplicationRecord
   self.primary_key = 'id'
   has_many :episodes
   has_many :reviews
+  has_many :likes, as: :likeable 
   validates :rss_feed_link, :title, presence: true
   validates :rss_feed_link, uniqueness: true
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :username, presence: true
 end
