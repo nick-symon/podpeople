fdd1caed9c488081d78f3a81be3f6993ad89f767
nick-symon <symon.nick@gmail.com>
2021-06-01 18:02:40 -0500

    base test case for lists, list joins

--- test/models/list_join_test.rb
+++ test/models/list_join_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ListJoinTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid list join" do
+    list_join = list_joins(:best_pods_join)
+    assert list_join.valid?
+  end
 end
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid list" do
+    list = lists(:best_pods)
+    assert list.valid?
+  end
 end
