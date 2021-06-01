d9ac158d21c4e183ca10f2db0f5db5d9892cf3c5
nick-symon <symon.nick@gmail.com>
2021-06-01 09:36:14 -0500

    associate like with reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :podcast
   belongs_to :profile
+  has_many :likes, as: :likeable
   validates_associated :podcast, :profile 
   validates :podcast, :profile, :rating, presence: true
   validates :rating, inclusion: {in: 1..5 }
