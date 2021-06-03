629b8e0f88262c30a4827ed803539fe8c5bbe2a3
nick-symon <symon.nick@gmail.com>
2021-06-03 17:39:59 -0500

    clean up like templating

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,8 @@ class Profile < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :likes, dependent: :destroy
   validates :username, presence: true
+
+  def likes_podcast?(podcast_id)
+    Like.where(profile_id: id, likeable_id: podcast_id, likeable_type: "Podcast").present?
+  end
 end
