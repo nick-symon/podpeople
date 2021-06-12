340d19d809060d93e60b4b7d5964777c3ebacf06
nick-symon <symon.nick@gmail.com>
2021-06-12 11:46:39 -0500

    wip commit. spider almost there!

--- lib/podcast_spider.rb
+++ lib/podcast_spider.rb
@@ -4,6 +4,23 @@ require 'kimurai'
 class PodcastSpider < Kimurai::Base
   @engine = :mechanize
   @start_urls = ["https://podcasts.apple.com/us/genre/podcasts/id26"]
+  @config = {
+    before_request: {
+      # Change user agent before each request:
+      # change_user_agent: true,
+      # Change proxy before each request:
+      # change_proxy: true,
+      # Clear all cookies and set default cookies (if provided) before each request:
+      clear_and_set_cookies: true,
+      # Process delay before each request:
+      # delay: 1..3
+    }
+  }
+  
+  def self.open_spider
+    @@podcast_ids = Podcast.pluck(:id)
+    @@podcasts_added = []
+  end
 
   def parse(response, url, data: {})
     #From main page we get links to all top-level genres
@@ -27,7 +44,6 @@ class PodcastSpider < Kimurai::Base
 
   def parse_podcast(response, url:, **data)
 
-    puts url
     def strip_id_from_link(link)
       link_list = link.split('/')
       raw_id = link_list[-1]
@@ -38,7 +54,11 @@ class PodcastSpider < Kimurai::Base
     response.css('div#selectedcontent a').each do |link|
       id = strip_id_from_link(link['href'])
       url = "https://itunes.apple.com/lookup?id=#{id}"
-      browser.visit(url)
+      if @@podcast_ids.include?(id)
+        return 
+      end
+      browser.visit(url) 
+      # browser.visit(url) unless @@podcast_ids.include?(id)
       parse_lookup(browser.current_response, url:url, **{id:id})
     end
     
@@ -50,34 +70,45 @@ class PodcastSpider < Kimurai::Base
     feed_url = results_hash['feedUrl']
     genres = results_hash['genres']
     podcast_name = results_hash['collectionName']
-    parse_rss(url: feed_url, **{id: data[:id], genres: genres}) 
+    if feed_url.nil?
+      return
+    else
+      parse_rss(url: feed_url, **{id: data[:id], genres: genres}) 
+    end
     # browser.visit(feed_url)
     # parse_final(browser.current_response, url: feed_url, **{id: data[:id], genres: genres}) 
   end
 
   def parse_rss(url:, **data)
-    feed = RSS::Parser.parse(url)
+    feed = RSS::Parser.parse(url, do_validate=false)
     podcast = {}
     podcast["id"] = data[:id]
     podcast["title"] = feed.channel.title 
-    podcast["subtitle"] = feed.channel.itunes_subtitle 
+    podcast["subtitle"] = feed.channel.itunes_subtitle || ''
     podcast["rss_feed_link"] = url
-    podcast["link"] = feed.channel.link 
+    podcast["link"] = feed.channel.link || ''
     podcast["itunes_categories"] = data[:genres]
-    podcast["image_url"] = feed.channel.image.url  
-    podcast["image_title"] = feed.channel.image.title 
+    podcast["image_url"] = feed.channel.image&.url || ''
+    podcast["image_title"] = feed.channel.image&.title || ''
     # thikn we should run below through a parse date fxn
-    podcast["last_build_date"] = feed.channel.lastBuildDate  
-    podcast["author_name"] = feed.channel.itunes_author   
-    podcast["description"] = feed.channel.description  
-    podcast["language"] = feed.channel.language  
-    podcast["itunes_summary"] = feed.channel.itunes_summary  
-    podcast["pub_date"] = feed.channel.pubDate  
+    podcast["last_build_date"] = feed.channel.lastBuildDate || ''
+    podcast["author_name"] = feed.channel.itunes_author || ''
+    podcast["description"] = feed.channel.description || ''
+    podcast["language"] = feed.channel.language || ''
+    podcast["itunes_summary"] = feed.channel.itunes_summary || ''
+    podcast["pub_date"] = feed.channel.pubDate || ''
+    podcast["slug"] = ''
     # we can extraxt text from xml using approach below
     # basic use the poddracer script but replace when neede
     # make sure we register namespcaes
     # note that name spacing doesn't currently work and a lot of attirubres aren't getting picked up
-    puts podcast
+    # puts podcast
+    p = Podcast.create(podcast)
+    @@podcasts_added << p
+  end
+
+  def self.close_spider
+    puts "#{@@podcasts_added.count} podcasts added"
   end
 
 end
