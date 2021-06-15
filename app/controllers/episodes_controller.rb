3935b4f101f239234afdfa74c121b5db2da563b0
nick-symon <symon.nick@gmail.com>
2021-06-15 09:20:27 -0500

    wip episode controller

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,16 @@
 class EpisodesController < ApplicationController
   def index
+    # get the podcast id from params
+    # get count of episodes, compare to rss feed count
+    # if they are equal, just load the episodes (jump to finish)
+    # otherwise, run the podcsat update helper
+    # then load podcasts
+    @podcast = Podcast.find(params[:id])
+    ep_builder = EpisodeBuilder.new(@podcast.rss_feed_link, @podcast.id)
+    if ep_builder.build_episodes?
+      ep_builder.build_episodes
+    end
+    @episodes = @podcast.episodes
+    # return js.erb file where we insert the episode partials
   end
 end
